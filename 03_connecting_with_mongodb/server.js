607c036c1673115c4c16c4f31c2cdff209459701
deependujha <deependujha21@gmail.com>
2022-11-02 12:09:56 +0530

    fetching data from the database and manipulation can be done and reponse can be sent to graphql queries and mutations

--- 03_connecting_with_mongodb/server.js
+++ 03_connecting_with_mongodb/server.js
@@ -1,25 +1,8 @@
-import { ApolloServer, gql } from 'apollo-server';
-import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 import './mongodb/conn.js';
-
-// here we are defining our 'GraphQL schemas'
-const typeDefs = gql`
-	type Query {
-		greet: String
-		bye: String
-	}
-`;
-// here we are defining our resolvers
-const resolvers = {
-	Query: {
-		greet: () => {
-			return 'hello world';
-		},
-		bye: () => {
-			return 'bye bye! Happy learning.';
-		},
-	},
-};
+import typeDefs from './schemaGQL.js';
+import resolvers from './resolvers.js';
+import { ApolloServer } from 'apollo-server';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 
 // here we are defining our server
 const server = new ApolloServer({
@@ -29,6 +12,6 @@ const server = new ApolloServer({
 });
 
 // here we are passing 6969 port no. If we donot pass it, it will run on 4000 port by default.
-server.listen(6969).then(({ url }) => {
+server.listen().then(({ url }) => {
 	console.log(`🚀 Server ready at ${url}`);
 });
